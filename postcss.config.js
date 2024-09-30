4d5f775e01cde54fb56085a1f63e6f532acf3339
Corentin Bessire <corentin.bessire@liip.ch>
2024-09-30 16:51:55 +0200

    Initial commit

---
+++ postcss.config.js
@@ -0,0 +1,8 @@
+let tailwindcss = require('tailwindcss');
+module.exports = {
+  plugins: [
+    require('postcss-import'),
+    require('tailwindcss/nesting'),
+    tailwindcss('./tailwind.config.js'),
+    require('autoprefixer')
+] }
---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  mode: 'jit',
+  content: [
+    './templates/**/*.html.twig',
+    './components/**/*.twig',
+    './js/**/*.js',
+  ]
+};

2bcee316787e8f5fda7f26955611a328b75e0c9d
Corentin Bessire <corentin.bessire@liip.ch>
2024-10-24 10:03:30 +0200

    Rename to TADstack

--- README.md
+++ README.md
@@ -1,9 +1,12 @@
-tailwind_starterkit theme, generated from starterkit_theme. Additional information on generating themes can be found in the [Starterkit documentation](https://www.drupal.org/docs/core-modules-and-themes/core-themes/starterkit-theme).
+Base Theme inspired by the TALLstack but for Drupal.
+
+Comes with :
+- TailwindCSS
+- AlpinJS
+
 
 ## Set up
-
 Prerequisite : `rename` (`brew install rename` // `sudo apt-get install rename`)
-
 
 Replace `themeName` by the name of the theme you want to setup.
 
---
+++ setup.sh
@@ -0,0 +1,33 @@
+
+#!/bin/bash
+
+show_help() {
+    echo "Usage: ./setup.sh themeName"
+    echo ""
+    echo "Options:"
+    echo "  themeName        The name of the Theme to create."
+}
+
+# Check if the first argument is --help
+if [[ "$1" == "--help" ]]; then
+    show_help
+    exit 0
+fi
+
+themeName=$1
+theme_directory=$(pwd)
+theme_location="$(dirname "$theme_directory")"
+
+# ------------------------------------------------------------------------------------------------------
+echo "🔗 Removing Git references..."
+rm -rf $theme_directory/.git  > /dev/null 2>&1
+
+# ------------------------------------------------------------------------------------------------------
+echo "🛠️ Renaming Theme..."
+
+find . -type f -name '*.yml' -exec sed -i '' s/tadstack/$theme_name/g {} +  > /dev/null 2>&1
+find . -type f -name '*.theme' -exec sed -i '' s/tadstack/$theme_name/g {} +  > /dev/null 2>&1
+find . -name "*tadstack.*" -exec rename -s 'tadstack' $theme_name  -v {} ";"  > /dev/null 2>&1
+cd $theme_location
+mv $theme_directory $theme_location/$theme_name  > /dev/null 2>&1
+cd $theme_location/$theme_name

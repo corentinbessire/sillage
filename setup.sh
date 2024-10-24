946d3a8fff842df307b5fc21a1f048821d8c6cce
Corentin Bessire <corentin.bessire@liip.ch>
2024-10-24 10:42:46 +0200

    Fix Search and replace for json files

--- setup.sh
+++ setup.sh
@@ -30,7 +30,7 @@ echo "🛠️ Renaming Theme..."
 #Search and Replace in Files
 find . -type f -name '*.yml' -exec sed -i '' 's/tadstack/'$theme_name'/g' {} \;
 find . -type f -name '*.theme' -exec sed -i '' 's/tadstack/'$theme_name'/g' {} \;
-find . -type f -name '*.yml' -exec sed -i '' 's/tadstack/'$theme_name'/g' {} \;
+find . -type f -name '*.json' -exec sed -i '' 's/tadstack/'$theme_name'/g' {} \;
 
 #Renam,ing Files
 find . -name "*tadstack.*" -exec rename -s 'tadstack' $theme_name {} ";" # > /dev/null 2>&1

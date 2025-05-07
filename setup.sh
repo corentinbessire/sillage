5143b895be558f72a9c6f216bc1b65b3e060736a
Corentin Bessire <corentin.bessire@liip.ch>
2025-05-07 08:23:58 +0200

    Rename Starter theme to sillage

--- setup.sh
+++ setup.sh
@@ -25,16 +25,17 @@ rm -rf $theme_directory/.git # > /dev/null 2>&1
 
 # ------------------------------------------------------------------------------------------------------
 echo "🛠️ Renaming Theme..."
-
-
 #Search and Replace in Files
 find . -type f -name '*.yml' -exec sed -i '' 's/sillage/'$theme_name'/g' {} \;
 find . -type f -name '*.theme' -exec sed -i '' 's/sillage/'$theme_name'/g' {} \;
 find . -type f -name '*.json' -exec sed -i '' 's/sillage/'$theme_name'/g' {} \;
 find . -type f -name '*.twig' -exec sed -i '' 's/sillage/'$theme_name'/g' {} \;
+find . -type f -name 'ddev_commands/*' -exec sed -i '' 's/sillage/'$theme_name'/g' {} \;
+find . -type f -name 'ddev_commands/*' -exec sed -i '' 's/Sillage/'$theme_name'/g' {} \;
 
 #Renam,ing Files
 find . -name "*sillage.*" -exec rename -s 'sillage' $theme_name {} ";" # > /dev/null 2>&1
+find . -name "*sillage:*" -exec rename -s 'sillage' $theme_name {} ";" # > /dev/null 2>&1
 
 #renaming Directory
 cd $theme_location
